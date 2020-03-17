52670f74ab34c8daf8314792e8c88d4318de55b2
NickVeliki <nickveliki@gmail.com>
2020-03-17 22:06:50 +0100

    init

---
+++ index.js
@@ -0,0 +1,39 @@
+const crypto = require("crypto");
+const fs = require("fs");
+const path = require("path");
+let publicKey;
+let privateKey;
+
+module.exports={
+    setup:(location)=>{
+        location = path.join(__dirname.replace(`node_modules${path.sep}verlikify${path.sep}`,""), location);
+        if (!fs.existsSync(location)){
+            fs.writeFileSync(location, JSON.stringify(crypto.generateKeyPairSync("rsa", {
+                modulusLength: 4096, 
+                publicKeyEncoding: {
+                    type: "spki",
+                    format:"pem"
+                },
+                privateKeyEncoding:{
+                    type: "pkcs8",
+                    format: "pem"
+                }
+            })))
+        }
+        const res = JSON.parse(fs.readFileSync(location).toString());
+        publicKey=res.publicKey;
+        privateKey=res.privateKey;
+    },
+    sign:(sig)=>{
+    const signer = crypto.createSign("SHA256");
+    signer.write(sig);
+    signer.end();
+    return signer.sign(privateKey).toString("base64");
+},
+verify:(sig, comp)=>{
+    const verifier = crypto.createVerify("SHA256");
+    verifier.write(comp);
+    verifier.end();
+    return verifier.verify(publicKey, Buffer.from(sig, "base64"));
+}
+}
